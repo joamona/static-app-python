826f4b47a826181520639848fd7d3a41eb5cd317
joamona <joamona@cgf.upv.es>
2024-04-22 16:29:07 +0200

    first commit

---
+++ app-python/calculate.py
@@ -0,0 +1,4 @@
+print("I execute a complicate remote sensing algorith")
+print("Entry data in the folder ./img.")
+print("Output data in the folder ./results")
+print("The container is stopped after the calculation")
